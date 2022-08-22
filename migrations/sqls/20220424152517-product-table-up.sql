e797f379b08df0accce023e19b9a122a24020324
heshamamoudi <hesham69mez@gmail.com>
2022-08-22 03:29:47 +0300

    front end commit

--- migrations/sqls/20220424152517-product-table-up.sql
+++ migrations/sqls/20220424152517-product-table-up.sql
@@ -1,4 +1,4 @@
 CREATE TABLE products (id SERIAL PRIMARY KEY,
             name VARCHAR(64) NOT NULL,
             price integer NOT NULL,
-            category VARCHAR(20));
+            category VARCHAR(20),urlimage text);
