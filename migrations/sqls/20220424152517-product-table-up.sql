6fdfb0496000ba59c49802ad76d6e153c9fe1ea7
heshamamoudi <hesham69mez@gmail.com>
2022-08-22 03:48:58 +0300

    insertion into products table

--- migrations/sqls/20220424152517-product-table-up.sql
+++ migrations/sqls/20220424152517-product-table-up.sql
@@ -2,3 +2,5 @@ CREATE TABLE products (id SERIAL PRIMARY KEY,
             name VARCHAR(64) NOT NULL,
             price integer NOT NULL,
             category VARCHAR(20),urlimage text);
+
+INSERT INTO products(name , price, category , urlimage) VALUES ('pr1',300,'electronics','http://localhost:5000/images/pr1.jpg');
